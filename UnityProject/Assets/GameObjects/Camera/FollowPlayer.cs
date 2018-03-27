8a6bfe4fc344add3d4c65798735ef25ee085cc1a
OrenjiAuris <aurimas.sernas@gmail.com>
2018-03-27 15:16:43 +0300

    PLA10 - make player tracking script for camera

---
+++ UnityProject/Assets/GameObjects/Camera/FollowPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public GameObject player;
+
+    public Vector3 offset;
+
+    void Update ()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ UnityProject/Assets/GameObjects/DoubleJumpPickUp/DoubleJumpPickUp.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleJumpPickUp : MonoBehaviour {
+
+    public GameObject blockade;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(blockade);
+    }
+}
