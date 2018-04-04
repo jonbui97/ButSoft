4f358b5cba92277b5f1e61472b4bf55229b7d025
Ričardas <noricardas@gmail.com>
2018-04-04 15:00:03 +0300

    Order of files rearanged

---
+++ UnityProject/Assets/Level/startlevel/Sripts/HUD.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HUD : MonoBehaviour
+{
+    //public List<Sprite> HeartSprites;
+    public Sprite[] HeartSprites;
+
+    public Image HeartUI;
+
+    public player player;
+
+    public static float offsetY = 5;
+    public static float offsetZ = -7;
+    public Vector3 offset = new Vector3(0, offsetY, offsetZ);
+
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+        HeartUI.sprite = HeartSprites[player.currHealth];
+    }
+}
---
+++ UnityProject/Assets/MainMenu/Scripts/SettingsMenu.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour {
+    Resolution[] resolutions;
+    public Dropdown resolutionDropdown;
+    public AudioMixer audioMixer;
+    public Slider masterVolSlider; public Slider musicVolSlider;
+    public Slider sfxVolSlider;
+    void Start()
+    {
+        masterVolSlider.value = PlayerPrefs.GetFloat("MasterVolume");
+        musicVolSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        sfxVolSlider.value = PlayerPrefs.GetFloat("SfxVolume");
+
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        int currentResolutionIndex = 0;
+
+        List<string> options = new List<string>();
+        for (int i = 0; i < resolutions.Length; i+=2)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("masterVolume", volume);
+        DontDestroyOnLoad(audioMixer);
+    }
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("musicVolume", volume);
+        DontDestroyOnLoad(audioMixer);
+    }
+    public void SetSoundEffectsVolume(float volume)
+    {
+        audioMixer.SetFloat("soundEffectsVolume", volume);
+        DontDestroyOnLoad(audioMixer);
+    }
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+    void Update()
+    {
+        PlayerPrefs.SetFloat("MasterVolume", masterVolSlider.value);
+        PlayerPrefs.SetFloat("MusicVolume", musicVolSlider.value);
+        PlayerPrefs.SetFloat("SfxVolume", sfxVolSlider.value);
+
+    }
+}
