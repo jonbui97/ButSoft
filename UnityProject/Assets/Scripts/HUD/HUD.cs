5e5208212acfb82dfd294dc47c567df00d2c8bae
Mindaugas Butkus <mr.mindaugas.butkus@gmail.com>
2018-04-05 11:17:29 +0300

    Cleaning project

---
+++ UnityProject/Assets/Scripts/HUD/HUD.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HUD : MonoBehaviour
+{
+    //public List<Sprite> HeartSprites;
+    public Sprite[] HeartSprites;
+
+    public Image HeartUI;
+
+    public player player;
+
+    public static float offsetY = 5;
+    public static float offsetZ = -7;
+    public Vector3 offset = new Vector3(0, offsetY, offsetZ);
+
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+        if (player.currHealth >= 0)
+        {
+            HeartUI.sprite = HeartSprites[player.currHealth];
+        }
+    }
+}
