a0cc7c7e16ebf9597a0a8fdb03c0d2c72d1e02df
OrenjiAuris <aurimas.sernas@gmail.com>
2018-04-23 21:19:23 +0300

    CAM1, CAM2; CAM1 - add look down script; CAM2 - add look up script

---
+++ UnityProject/Assets/Scripts/Camera/Camera_Controller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Controller : MonoBehaviour
+{
+
+    public GameObject player;
+    [SerializeField]
+    private float amount;
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private Vector3 offset;
+
+    void Update()
+    {
+        if (Input.GetAxis("Vertical") == 0)
+        {         
+            Vector3 target = new Vector3(player.transform.position.x + offset.x, player.transform.position.y + offset.y, player.transform.position.z + offset.z);
+            transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * moveSpeed);
+        }
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            Vector3 target = new Vector3(player.transform.position.x + offset.x, player.transform.position.y + (2 * offset.y) + amount, player.transform.position.z + offset.z);
+            transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * moveSpeed);
+        }
+
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            Vector3 target = new Vector3(player.transform.position.x + offset.x, player.transform.position.y - offset.y - amount, player.transform.position.z + offset.z);
+            transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * moveSpeed);
+        }
+    }
+}
