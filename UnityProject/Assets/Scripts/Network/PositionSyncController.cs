35e786a82ba2a2b9245fb03777620f3c85dd1d4e
Mindaugas Butkus <mr.mindaugas.butkus@gmail.com>
2018-05-08 09:20:02 +0300

    LOM7 - create smooth not local player (second player) movment.

---
+++ UnityProject/Assets/Scripts/Network/PositionSyncController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PositionSyncController : NetworkBehaviour
+{
+
+    [SyncVar] private Vector3 syncPos;
+
+    [SerializeField] private Transform myTransform;
+    [SerializeField] private float lerpRate = 15;
+
+    private void Update()
+    {
+        LerpPosition();
+    }
+
+    private void FixedUpdate()
+    {
+        TransmitPosition();
+    }
+
+    private void LerpPosition()
+    {
+        if (!isLocalPlayer)
+        {
+            myTransform.position = Vector3.Lerp(myTransform.position, syncPos, Time.deltaTime * lerpRate);
+        }
+    }
+
+    [Command]
+    private void CmdProvidePositionToServer(Vector3 pos)
+    {
+        syncPos = pos;
+    }
+
+    [ClientCallback]
+    private void TransmitPosition()
+    {
+        if (isLocalPlayer)
+        {
+            CmdProvidePositionToServer(myTransform.position);
+        }
+    }
+}
